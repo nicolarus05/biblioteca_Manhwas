c4e0d9fb68bb7d4c302127cd1a3967ed66e3a998
Nicolas <124302263+nicolarus05@users.noreply.github.com>
2025-01-23 19:09:49 +0100

    creacion de archivos y carpetas

---
+++ DB/conexion.php
@@ -0,0 +1,21 @@
+<?php
+// Configuración de la base de datos
+$host = '172.21.0.2'; // Cambiar si usas un servidor remoto
+$user = 'manhwas'; // Usuario de MySQL
+$password = '261205'; // Contraseña de MySQL
+$dbname = 'biblioteca_manhwas'; // Nombre de la base de datos
+
+try {
+    // Crear una nueva conexión PDO
+    $dsn = "mysql:host=$host;dbname=$dbname;charset=utf8mb4";
+    $pdo = new PDO($dsn, $user, $password);
+
+    // Configurar el modo de errores de PDO
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+    echo "Conexión exitosa a la base de datos.";
+} catch (PDOException $e) {
+    // Capturar errores de conexión
+    die("Error de conexión: " . $e->getMessage());
+}
+?>
